b49ef6c515c829f0e120b6338fe015118dd36127
Bradley Cushing <bradleycushing@gmail.com>
2023-11-20 09:50:49 -0500

    feat(gittuf): add gittuf branch permission with commit

--- README.md
+++ README.md
@@ -1,3 +1,48 @@
 # ssl-demo
 
 Demo integrating gittuf, SBOMit, and RSTUF
+
+## Demo steps
+
+- [x] Create a new temporary repository
+- [x] Delegate permission for branch updates on main
+- [ ] Update a binary changing content of output
+- [ ] Commit the change by the delegated user
+- [ ] Initialize a temporary bare repository
+- [ ] Link our local repository to the bare repository
+- [ ] Create in-toto policy metadata by Alice
+- [ ] Clone repository to prepare for change by Bob
+- [ ] Make change and create in-toto metadata for Bob
+- [ ] Push metadata to locally running RSTUF server
+- [ ] Client downloads in-toto metadata from RSTUF server
+- [ ] Client checks out code from temporary repository
+- [ ] Verify product on the client after downloading
+
+### Not handled yet
+
+- Using README.md instead of binary
+- Anything to do with SBOMMit
+- Anything to do with RSTUF
+
+## Environment
+
+A virtual Python enviroment is recommended
+
+## Requirements
+
+- gittuf
+- protobomit
+- RSTUF
+
+## Install
+
+``` sh
+# Install dependencies
+pip install -r requirements.txt
+```
+
+## Running the demo
+
+``` sh
+python run-demo.py
+```
---
+++ run-demo.py
@@ -0,0 +1,238 @@
+#!/usr/bin/env python
+
+import gnupg
+import os
+import shlex
+import shutil
+import subprocess
+import sys
+import tempfile
+
+NO_PROMPT = False
+REQUIRED_BINARIES = ["git", "gittuf", "gpg"]
+
+def check_binaries():
+    for p in REQUIRED_BINARIES:
+        if not shutil.which(p):
+            raise Exception(f"required command {p} not found")
+
+def prompt_key(prompt):
+    if NO_PROMPT:
+        print("\n" + prompt)
+        return
+    inp = False
+    while inp != "":
+        try:
+            inp = input(f"\n{prompt} -- press any key to continue")
+        except Exception:
+            pass
+
+def display_command(cmd):
+    print(f"[{os.getcwd()}] $ {cmd}")
+
+def setup_gpg_keys(gpg_dir):
+    os.mkdir(gpg_dir)
+
+    gpg = gnupg.GPG(gnupghome=gpg_dir)
+    gpg.encoding = "utf-8"
+
+    key_params = gpg.gen_key_input(
+        key_type="RSA",
+        key_length=1024,
+        name_real="Authorized Developer",
+        name_email="gittuf.authorized@example.com",
+        no_protection=True
+    )
+    authorized_gpg_key = gpg.gen_key(key_params)
+
+    key_params = gpg.gen_key_input(
+        key_type="RSA",
+        key_length=1024,
+        name_real="Unauthorized Developer",
+        name_email="gittuf.unauthorized@example.com",
+        no_protection=True
+    )
+    unauthorized_gpg_key = gpg.gen_key(key_params)
+
+    return authorized_gpg_key, unauthorized_gpg_key
+
+def run_demo():
+    current_dir = os.getcwd()
+    gpg_dir = "gpg-dir"
+    keys_dir = "keys"
+
+    tmp_dir = tempfile.TemporaryDirectory()
+    tmp_gpg_dir = os.path.join(tmp_dir.name, gpg_dir)
+    tmp_keys_dir = os.path.join(tmp_dir.name, keys_dir)
+    tmp_repo_dir = os.path.join(tmp_dir.name, "repo")
+
+    prompt_key("Create test GPG keys")
+    authorized_gpg_key, unauthorized_gpg_key = setup_gpg_keys(tmp_gpg_dir)
+    prompt_key(
+        "Created authorized key with fingerprint"
+        f" {authorized_gpg_key.fingerprint} and unauthorized key with"
+        f" fingerprint {unauthorized_gpg_key.fingerprint}"
+    )
+
+    shutil.copytree(os.path.join(current_dir, keys_dir), tmp_keys_dir)
+    os.mkdir(tmp_repo_dir)
+    os.chdir(tmp_repo_dir)
+
+    prompt_key("Initialize Git repository")
+    cmd = "git init -b main"
+    display_command(cmd)
+    subprocess.call(shlex.split(cmd))
+
+    prompt_key("Set repo config to use demo identity and test key")
+    cmd = f"git config --local user.signingkey {authorized_gpg_key.fingerprint}"
+    display_command(cmd)
+    subprocess.call(shlex.split(cmd))
+    cmd = f"git config --local user.name gittuf-demo"
+    display_command(cmd)
+    subprocess.call(shlex.split(cmd))
+    cmd = f"git config --local user.email gittuf.demo@example.com"
+    display_command(cmd)
+    subprocess.call(shlex.split(cmd))
+
+    prompt_key("Set GNUPGHOME and PAGER")
+    os.environ["GNUPGHOME"] = tmp_gpg_dir
+    os.environ["PAGER"] = "cat"
+    display_command(f"export GNUPGHOME={tmp_gpg_dir}")
+    display_command(f"export PAGER=cat")
+
+    prompt_key("Initialize gittuf root of trust")
+    cmd = "gittuf trust init -k ../keys/root"
+    display_command(cmd)
+    subprocess.call(shlex.split(cmd))
+
+    prompt_key("Add policy key to gittuf root of trust")
+    cmd = ("gittuf trust add-policy-key"
+        " -k ../keys/root"
+        " --policy-key ../keys/targets.pub"
+    )
+    display_command(cmd)
+    subprocess.call(shlex.split(cmd))
+
+    prompt_key("Initialize policy")
+    cmd = "gittuf policy init -k ../keys/targets"
+    display_command(cmd)
+    subprocess.call(shlex.split(cmd))
+
+    prompt_key("Add rule to protect the main branch")
+    cmd = ("gittuf policy add-rule"
+        " -k ../keys/targets"
+        " --rule-name 'protect-main'"
+        " --rule-pattern git:refs/heads/main"
+        f" --authorize-key gpg:{authorized_gpg_key.fingerprint}"
+    )
+    display_command(cmd)
+    subprocess.call(shlex.split(cmd))
+
+    prompt_key("Make change to repo's main branch")
+    display_command("echo 'Hello, world!' > README.md")
+    with open("README.md", "w") as fp:
+        fp.write("Hello, world!\n")
+    cmd = "git add README.md"
+    display_command(cmd)
+    subprocess.call(shlex.split(cmd))
+    cmd = "git commit -m 'Initial commit'"
+    display_command(cmd)
+    subprocess.call(shlex.split(cmd))
+
+    prompt_key("Record change to main in RSL")
+    cmd = "gittuf rsl record main"
+    display_command(cmd)
+    subprocess.call(shlex.split(cmd))
+    cmd = "git show refs/gittuf/reference-state-log"
+    display_command(cmd)
+    subprocess.call(shlex.split(cmd))
+
+    prompt_key("Verify branch protection for this change")
+    cmd = "gittuf verify-ref -f main"
+    display_command(cmd)
+    subprocess.run(shlex.split(cmd), check=True)
+
+    prompt_key("gittuf's verification succeeded!")
+
+    prompt_key("Verify commit signature using gittuf policy")
+    cmd = "gittuf verify-commit HEAD"
+    display_command(cmd)
+    subprocess.run(shlex.split(cmd), check=True)
+
+    prompt_key(
+        "gittuf's policy recognizes the signing key and"
+        " confirms it's a good signature!"
+    )
+
+    prompt_key("Update repo config to use unauthorized key")
+    cmd = f"git config --local user.signingkey {unauthorized_gpg_key.fingerprint}"
+    display_command(cmd)
+    subprocess.call(shlex.split(cmd))
+
+    prompt_key("Make unauthorized change to repo's main branch")
+    display_command("echo 'This is not allowed!' >> README.md")
+    with open("README.md", "a") as fp:
+        fp.write("This is not allowed!\n")
+    cmd = "git add README.md"
+    display_command(cmd)
+    subprocess.call(shlex.split(cmd))
+    cmd = "git commit -m 'Update README.md'"
+    display_command(cmd)
+    subprocess.call(shlex.split(cmd))
+
+    prompt_key("Record change to main in RSL")
+    cmd = "gittuf rsl record main"
+    display_command(cmd)
+    subprocess.call(shlex.split(cmd))
+    cmd = "git show refs/gittuf/reference-state-log"
+    display_command(cmd)
+    subprocess.call(shlex.split(cmd))
+
+    prompt_key("Verify branch protection for this change")
+    cmd = "gittuf verify-ref -f main"
+    display_command(cmd)
+    subprocess.call(shlex.split(cmd))
+
+    prompt_key("gittuf detected a violation of the branch protection rule!")
+
+    prompt_key("Verify commit signature using gittuf policy")
+    cmd = "gittuf verify-commit HEAD"
+    display_command(cmd)
+    subprocess.run(shlex.split(cmd), check=True)
+
+    prompt_key("gittuf's policy does not recognize the signing key!")
+
+    prompt_key("Rewind to last good state to test attestation")
+    cmd = "git reset --hard HEAD~1"
+    display_command(cmd)
+    subprocess.call(shlex.split(cmd))
+    cmd = "git update-ref refs/gittuf/reference-state-log refs/gittuf/reference-state-log~1"
+    display_command(cmd)
+    subprocess.call(shlex.split(cmd))
+    cmd = f"git config --local user.signingkey {authorized_gpg_key.fingerprint}"
+    display_command(cmd)
+    subprocess.call(shlex.split(cmd))
+
+    prompt_key("Initialize bare git repository")
+    tmp_bare_repo_dir = os.path.join(tmp_dir.name, "bare-repo")
+    os.mkdir(tmp_bare_repo_dir)
+    os.chdir(tmp_bare_repo_dir)
+    cmd = "git --bare init"
+    display_command(cmd)
+    subprocess.call(shlex.split(cmd))
+
+    prompt_key("Push changes to bare git repository")
+    os.chdir(tmp_repo_dir)
+    cmd = "git remote add origin ../bare-repo"
+    display_command(cmd)
+    subprocess.call(shlex.split(cmd))
+    cmd = "git push origin main"
+    display_command(cmd)
+    subprocess.call(shlex.split(cmd))
+
+if __name__ == "__main__":
+    if len(sys.argv) > 1:
+        if sys.argv[1] == "--no-prompt":
+            NO_PROMPT = True
+    check_binaries()
+    run_demo()
